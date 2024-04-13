4bb24275da4661fda51772ddc77a7a02085952b4
imdan <imdan@umich.edu>
2024-04-13 11:53:50 -0400

    Setup files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Noted.</h2>
+      <form>
+        <input type='file' />
+        <button type="submit">Upload</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
