6f37511138f90d5d44381d7e6c17ca56ae10e0d4
imdan <imdan@umich.edu>
2024-04-13 12:02:12 -0400

    Added dropzone components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+import { useCallback } from 'react';
 import './App.css';
+import { useDropzone } from 'react-dropzone';
 
 function App() {
+
+  const onDrop = useCallback(acceptedFiles => {
+    // do something
+  }, [])
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
     <div className="App">
       <h2>Noted.</h2>
-      <form>
-        <input type='file' />
-        <button type="submit">Upload</button>
-      </form>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+      </div>
     </div>
   );
 }
